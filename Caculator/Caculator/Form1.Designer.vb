80d71f22adce37c427c246460ed3c49bf7e0decf
JBROSFC <62401313+JBROSFC@users.noreply.github.com>
2020-11-16 11:40:15 +0000

    New Upload
    V2

---
+++ Caculator/Caculator/Form1.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNumberOne = New System.Windows.Forms.TextBox()
+        Me.txtNumberTwo = New System.Windows.Forms.TextBox()
+        Me.lblOperator = New System.Windows.Forms.Label()
+        Me.btnNo1 = New System.Windows.Forms.Button()
+        Me.btnNo3 = New System.Windows.Forms.Button()
+        Me.btnNo2 = New System.Windows.Forms.Button()
+        Me.btnNo5 = New System.Windows.Forms.Button()
+        Me.btnNo6 = New System.Windows.Forms.Button()
+        Me.btnNo4 = New System.Windows.Forms.Button()
+        Me.btnNo8 = New System.Windows.Forms.Button()
+        Me.btnNo9 = New System.Windows.Forms.Button()
+        Me.btnNo7 = New System.Windows.Forms.Button()
+        Me.btnEquals = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.txtAnswer = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNumberOne
+        '
+        Me.txtNumberOne.Location = New System.Drawing.Point(34, 27)
+        Me.txtNumberOne.Name = "txtNumberOne"
+        Me.txtNumberOne.Size = New System.Drawing.Size(100, 26)
+        Me.txtNumberOne.TabIndex = 0
+        '
+        'txtNumberTwo
+        '
+        Me.txtNumberTwo.Location = New System.Drawing.Point(203, 27)
+        Me.txtNumberTwo.Name = "txtNumberTwo"
+        Me.txtNumberTwo.Size = New System.Drawing.Size(100, 26)
+        Me.txtNumberTwo.TabIndex = 1
+        '
+        'lblOperator
+        '
+        Me.lblOperator.AutoSize = True
+        Me.lblOperator.Location = New System.Drawing.Point(140, 33)
+        Me.lblOperator.Name = "lblOperator"
+        Me.lblOperator.Size = New System.Drawing.Size(0, 20)
+        Me.lblOperator.TabIndex = 2
+        '
+        'btnNo1
+        '
+        Me.btnNo1.Location = New System.Drawing.Point(34, 84)
+        Me.btnNo1.Name = "btnNo1"
+        Me.btnNo1.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo1.TabIndex = 3
+        Me.btnNo1.Text = "1"
+        Me.btnNo1.UseVisualStyleBackColor = True
+        '
+        'btnNo3
+        '
+        Me.btnNo3.Location = New System.Drawing.Point(154, 84)
+        Me.btnNo3.Name = "btnNo3"
+        Me.btnNo3.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo3.TabIndex = 4
+        Me.btnNo3.Text = "3"
+        Me.btnNo3.UseVisualStyleBackColor = True
+        '
+        'btnNo2
+        '
+        Me.btnNo2.Location = New System.Drawing.Point(94, 84)
+        Me.btnNo2.Name = "btnNo2"
+        Me.btnNo2.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo2.TabIndex = 5
+        Me.btnNo2.Text = "2"
+        Me.btnNo2.UseVisualStyleBackColor = True
+        '
+        'btnNo5
+        '
+        Me.btnNo5.Location = New System.Drawing.Point(94, 143)
+        Me.btnNo5.Name = "btnNo5"
+        Me.btnNo5.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo5.TabIndex = 8
+        Me.btnNo5.Text = "5"
+        Me.btnNo5.UseVisualStyleBackColor = True
+        '
+        'btnNo6
+        '
+        Me.btnNo6.Location = New System.Drawing.Point(154, 143)
+        Me.btnNo6.Name = "btnNo6"
+        Me.btnNo6.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo6.TabIndex = 7
+        Me.btnNo6.Text = "6"
+        Me.btnNo6.UseVisualStyleBackColor = True
+        '
+        'btnNo4
+        '
+        Me.btnNo4.Location = New System.Drawing.Point(34, 143)
+        Me.btnNo4.Name = "btnNo4"
+        Me.btnNo4.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo4.TabIndex = 6
+        Me.btnNo4.Text = "4"
+        Me.btnNo4.UseVisualStyleBackColor = True
+        '
+        'btnNo8
+        '
+        Me.btnNo8.Location = New System.Drawing.Point(94, 202)
+        Me.btnNo8.Name = "btnNo8"
+        Me.btnNo8.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo8.TabIndex = 11
+        Me.btnNo8.Text = "8"
+        Me.btnNo8.UseVisualStyleBackColor = True
+        '
+        'btnNo9
+        '
+        Me.btnNo9.Location = New System.Drawing.Point(154, 202)
+        Me.btnNo9.Name = "btnNo9"
+        Me.btnNo9.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo9.TabIndex = 10
+        Me.btnNo9.Text = "9"
+        Me.btnNo9.UseVisualStyleBackColor = True
+        '
+        'btnNo7
+        '
+        Me.btnNo7.Location = New System.Drawing.Point(34, 202)
+        Me.btnNo7.Name = "btnNo7"
+        Me.btnNo7.Size = New System.Drawing.Size(54, 53)
+        Me.btnNo7.TabIndex = 9
+        Me.btnNo7.Text = "7"
+        Me.btnNo7.UseVisualStyleBackColor = True
+        '
+        'btnEquals
+        '
+        Me.btnEquals.Location = New System.Drawing.Point(214, 143)
+        Me.btnEquals.Name = "btnEquals"
+        Me.btnEquals.Size = New System.Drawing.Size(75, 112)
+        Me.btnEquals.TabIndex = 12
+        Me.btnEquals.Text = "="
+        Me.btnEquals.UseVisualStyleBackColor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Location = New System.Drawing.Point(214, 84)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(75, 53)
+        Me.btnPlus.TabIndex = 13
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'txtAnswer
+        '
+        Me.txtAnswer.Location = New System.Drawing.Point(189, 277)
+        Me.txtAnswer.Name = "txtAnswer"
+        Me.txtAnswer.Size = New System.Drawing.Size(100, 26)
+        Me.txtAnswer.TabIndex = 14
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(126, 280)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 20)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Answer"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(327, 315)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtAnswer)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btnEquals)
+        Me.Controls.Add(Me.btnNo8)
+        Me.Controls.Add(Me.btnNo9)
+        Me.Controls.Add(Me.btnNo7)
+        Me.Controls.Add(Me.btnNo5)
+        Me.Controls.Add(Me.btnNo6)
+        Me.Controls.Add(Me.btnNo4)
+        Me.Controls.Add(Me.btnNo2)
+        Me.Controls.Add(Me.btnNo3)
+        Me.Controls.Add(Me.btnNo1)
+        Me.Controls.Add(Me.lblOperator)
+        Me.Controls.Add(Me.txtNumberTwo)
+        Me.Controls.Add(Me.txtNumberOne)
+        Me.Name = "Form1"
+        Me.Text = "JBR Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumberOne As TextBox
+    Friend WithEvents txtNumberTwo As TextBox
+    Friend WithEvents lblOperator As Label
+    Friend WithEvents btnNo1 As Button
+    Friend WithEvents btnNo3 As Button
+    Friend WithEvents btnNo2 As Button
+    Friend WithEvents btnNo5 As Button
+    Friend WithEvents btnNo6 As Button
+    Friend WithEvents btnNo4 As Button
+    Friend WithEvents btnNo8 As Button
+    Friend WithEvents btnNo9 As Button
+    Friend WithEvents btnNo7 As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents txtAnswer As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Caculator/Caculator/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Dim numberOne As Integer
+    Dim numberTwo As Integer
+    Dim firstNumberCheck As Boolean = True
+    Dim operatorSymbol As Char
+    Private Sub btnNo1_Click(sender As Object, e As EventArgs) Handles btnNo1.Click
+        If firstNumberCheck = True Then
+            txtNumberOne.Text = txtNumberOne.Text & 1
+            numberOne = txtNumberOne.Text
+        Else
+            txtNumberTwo.Text = txtNumberTwo.Text & 1
+            numberTwo = txtNumberTwo.Text
+        End If
+
+    End Sub
+
+    Private Sub btnNo2_Click(sender As Object, e As EventArgs) Handles btnNo2.Click
+        txtNumberOne.Text = 2
+    End Sub
+
+    Private Sub btnNo3_Click(sender As Object, e As EventArgs) Handles btnNo3.Click
+        txtNumberOne.Text = 3
+    End Sub
+
+    Private Sub btnNo4_Click(sender As Object, e As EventArgs) Handles btnNo4.Click
+        txtNumberOne.Text = 4
+    End Sub
+
+    Private Sub btnNo5_Click(sender As Object, e As EventArgs) Handles btnNo5.Click
+        txtNumberOne.Text = 5
+    End Sub
+
+    Private Sub btnNo6_Click(sender As Object, e As EventArgs) Handles btnNo6.Click
+        txtNumberOne.Text = 6
+    End Sub
+
+    Private Sub btnNo7_Click(sender As Object, e As EventArgs) Handles btnNo7.Click
+        txtNumberOne.Text = 7
+    End Sub
+
+    Private Sub btnNo8_Click(sender As Object, e As EventArgs) Handles btnNo8.Click
+        txtNumberOne.Text = 8
+    End Sub
+
+    Private Sub btnNo9_Click(sender As Object, e As EventArgs) Handles btnNo9.Click
+        txtNumberOne.Text = 9
+    End Sub
+
+    Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+        firstNumberCheck = False
+        lblOperator.Text = "+"
+        operatorSymbol = "+"
+    End Sub
+    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+
+        If operatorSymbol = "+" Then
+            txtAnswer.Text = numberOne + numberTwo
+        End If
+
+
+    End Sub
+End Class
